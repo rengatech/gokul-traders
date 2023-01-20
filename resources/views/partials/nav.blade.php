1e6f214304e826db1052a6b55a8d296dc24bfbdb
satheesh kumar <satheeskumarin2@gmail.com>
2023-01-20 19:12:07 +0530

    megamenu

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -59,51 +59,52 @@
                 
                 {{-- dropdown --}}
                 
-                <nav class="navbar navbar-expand-lg bg-gray-50 relative flex items-center justify-between">
-                  <div class="px-6">
+                 <div class="navbar navbar-expand-lg bg-gray-50 relative flex items-center justify-between">
+                  <div class="">
                     <button class="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContentX" aria-controls="navbarSupportedContentX" aria-expanded="false" aria-label="Toggle navigation">
                       <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                         <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
                       </svg>
                     </button>
                     <div class="navbar-collapse collapse grow items-center" id="navbarSupportedContentX">
-                      <ul class="navbar-nav mr-auto flex flex-row">
+                      <ul class="navbar-nav flex flex-row">
                         <li class="nav-item dropdown static">
-                          <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out dropdown-toggle flex items-center whitespace-nowrap" href="#" data-mdb-ripple="true" data-mdb-ripple-color="light" type="button" id="dropdownMenuButtonX" data-bs-toggle="dropdown"
+                          <a class="block text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out dropdown-toggle flex items-center whitespace-nowrap" href="#"
+                           type="button" id="dropdownMenuButtonX" data-bs-toggle="dropdown"
                           aria-expanded="false">Mega menu
                             <svg  aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" class="w-2 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                               <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                             </svg>
                           </a>
-                          <div class="dropdown-menu w-full mt-0 hidden shadow-lg bg-white absolute left-0 top-full" style="width: 400%;" aria-labelledby="dropdownMenuButtonX">
-                            <div class="px-6 lg:px-8 py-5">
-                              <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+                          <div class="dropdown-menu w-full mt-0 hidden shadow-lg bg-white absolute left-0 top-full" style="width: 650%; margin-top:20px;" aria-labelledby="dropdownMenuButtonX">
+                            <div class="">
+                              <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-15">
                                 <div class="bg-white text-gray-600">
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFm</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFctetur</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Chakkra</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFctetur</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
                                   <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFg elit</a>
                                 </div>
                                 <div class="bg-white text-gray-600">
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFuptas</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFis quo</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFiores</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFuptas</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFis quo</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFiores</a>
                                   <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFdolor</a>
                                 </div>
                                 <div class="bg-white text-gray-600">
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFato</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFm</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFato</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFm</a>
                                   <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
                                 </div>
                                 <div class="bg-white text-gray-600">
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFato</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
-                                  <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFm</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFato</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
+                                  <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFm</a>
                                   <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
                                 </div>
                               </div>
@@ -113,14 +114,11 @@
                       </ul>
                     </div>
                   </div>
-                </nav>
-            
+                </div> 
+                
                 {{-- dropdown --}}
-              
-              <a
-                href="/gallery"
-                class="  text-2xl font-semi-bold text-blue-800  hover:text-[#FF52B4]  btn1 btn-21 hover-slide-right  <?php echo $active_page === 'gallery' ? 'border-b-4 border-[#FF52B4] text-[#FF52B4]' : ''; ?>"><span>Gallery</span></a>
-              
+       
+
               
               <a
                 href="/contact-us"
@@ -216,6 +214,63 @@
                 >Products</a
               >
             </li>
+
+            <!-- mega menu -->
+            <div class=" navbar-expand-lg bg-gray-50 relative flex items-center justify-between">
+              <div class="">
+         
+                <div class="navbar-collapse collapse grow items-center" id="navbarSupportedContentX">
+                  <ul class="navbar-nav flex flex-row">
+                    <li class="nav-item dropdown static">
+                      <a class="block text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out dropdown-toggle flex items-center whitespace-nowrap" href="#"
+                       type="button" id="dropdownMenuButtonX" data-bs-toggle="dropdown"
+                      aria-expanded="false">Mega menu
+                        <svg  aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" class="w-2 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                          <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+                        </svg>
+                      </a>
+                      <div class="dropdown-menu w-full mt-0 hidden shadow-lg bg-white absolute left-0 top-full" style="width: 650%; margin-top:20px;" aria-labelledby="dropdownMenuButtonX">
+                        <div class="">
+                          <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-15">
+                            <div class="bg-white text-gray-600">
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Chakkra</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFctetur</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFg elit</a>
+                            </div>
+                            <div class="bg-white text-gray-600">
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFuptas</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFis quo</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFiores</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFdolor</a>
+                            </div>
+                            <div class="bg-white text-gray-600">
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFato</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFm</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
+                            </div>
+                            <div class="bg-white text-gray-600">
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFato</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF odio</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDFm</a>
+                              <a href="#!" aria-current="true" class="block px-6 py-2 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">AASDF</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div> 
+        
+            <!-- mega menu -->
+
             <li>
               <a
                 href="/gallery"
